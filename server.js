fe71d3ad6470a5491c279d3f9f590013d2e53f68
MaTu <ivancostilla17@gmail.com>
2022-10-17 21:09:32 -0300

    public + middleware

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+// Middleware
+app.use(express.static('public'))
 
 app.use('/',(req,res) => {
     res.send("Server on")
 })
 
-app.listen(PORT , () => {
+server.listen(PORT , () => {
     console.log("Server on in PORT : " ,PORT)
 })
 
